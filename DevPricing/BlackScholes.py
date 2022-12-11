ce11ef3364e7767f5bf2518db114737f10e7731a
guyuuuuu <yu.gu@nyu.edu>
2022-12-11 12:42:24 -0500

    [feat]binary call for mc,bs

--- DevPricing/BlackScholes.py
+++ DevPricing/BlackScholes.py
@@ -22,3 +22,23 @@ def EuroCall(S: float, K: float, T: float, sigma: float, r: float, y: float = 0,
     Nd2 = norm.cdf(d2)
     c = S * np.exp(-y) * Nd1 - K * np.exp(-r * (T - t)) * Nd2
     return c
+
+def BinaryCall(Q: float, S: float, K: float, T: float, sigma: float, r: float, y: float = 0, t: float = 0) -> float:
+    '''
+    S: Currunt Stock Price
+    K: Strike Price
+    T: Maturity Day, measured in years
+    sigma: Volatility, measured in precentage/100
+    r: risk-free rate, measured in precentage/100
+    y: Dividend Yield Rate (Continues), default to be zero
+    t: Current Time, default to be zero
+
+    Example:
+    BinaryCall(1, 80, 50, 1, 0.2, 0.05)
+    Option Pricing for a call mature in 1 year with no dividend yield,
+    strike price 50, currunt price 80, volatility at 20% and risk-free rate at 5%
+    '''    
+    d2 = (np.log(S / K) + (r - y - ((sigma ** 2) / 2) * (T- t))) / (sigma * ((T - t) ** 0.5))
+    Nd2 = norm.cdf(d2)
+    c = Q * np.exp(-r * (T - t)) * Nd2
+    return c
--- demo.py
+++ demo.py
@@ -1,8 +1,14 @@
 import DevPricing.MonteCarlo as mc
 import DevPricing.BlackScholes as bs
 
-
+print('Problem 1, 1-3')
 print(f'Analytic Solution: {bs.EuroCall(420, 400, 1, 0.2, 0.01)}')
 print(f'n=10000: {mc.EuroCall(420, 400, 1, 0.2, 0.01, 10000)}')
 print(f'n=100000: {mc.EuroCall(420, 400, 1, 0.2, 0.01, 100000)}')
 print(f'n=1000000: {mc.EuroCall(420, 400, 1, 0.2, 0.01, 1000000)}')
+
+print('Problem 1, 4-5, 7-8')
+print(f'Analytic Solution: {bs.BinaryCall(1, 420, 400, 1, 0.2, 0.01)}')
+print(f'n=10000: {mc.BinaryCall(1, 420, 400, 1, 0.2, 0.01, 10000)}')
+print(f'n=100000: {mc.BinaryCall(1, 420, 400, 1, 0.2, 0.01, 100000)}')
+print(f'n=1000000: {mc.BinaryCall(1, 420, 400, 1, 0.2, 0.01, 1000000)}')
